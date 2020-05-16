c749a8b3ead5185cc025c849a61ace81e084826e
projas <projas@kometsales.com>
2020-05-16 17:25:14 -0500

    first build

---
+++ build/precache-manifest.5bdcb3968a4f5ac26795cf3c03fc9ade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c400dc17f57a501a9f491922a522e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b635e001ed64a164fb3e",
+    "url": "/static/css/main.12b1ee9e.chunk.css"
+  },
+  {
+    "revision": "c744cd1efce88ef1c056",
+    "url": "/static/js/2.a03ace91.chunk.js"
+  },
+  {
+    "revision": "0776d88bf142a7918192885dc70c37d5",
+    "url": "/static/js/2.a03ace91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b635e001ed64a164fb3e",
+    "url": "/static/js/main.445824ab.chunk.js"
+  },
+  {
+    "revision": "4d91eba04aa52aeb2dfc",
+    "url": "/static/js/runtime-main.3c0326e9.js"
+  },
+  {
+    "revision": "bb0ac418c1c216a752ad9e16ffa0619d",
+    "url": "/static/media/footer_logo_white.bb0ac418.jpg"
+  }
+]);
