608a8bc6268cda2dc55869552f6f8b909202c725
Aron Eidelman <aron@eideldev.com>
2020-05-20 20:21:00 -0700

    remove inline styles
    these styles are no-op code, will focus on the component itself.

--- src/shamanic/page02.jsx
+++ src/shamanic/page02.jsx
@@ -36,7 +36,7 @@ function Page01( props ) {
              <CubeBanner />
         </section>
 
-        <section  style={ {  background:'#fff', height:'1200px'  } } id='sectionB'>
+        <section id='sectionB'>
              <CubeAbout2 />
         </section>
 
