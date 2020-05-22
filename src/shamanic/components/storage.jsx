ecaea0e371e96935166a39776cf73de92f12fcd7
projas <projas@kometsales.com>
2020-05-21 20:18:20 -0500

    fix url

--- src/shamanic/components/storage.jsx
+++ src/shamanic/components/storage.jsx
@@ -30,7 +30,7 @@ function Storage(){
         cta02:"Empieza tu Experiencia Sanadora",
         cta02_loading_message:"Cargando",
         video:"https://player.vimeo.com/video/402745799",    facebook:"https://www.facebook.com/The-Shift-Network-Espa%C3%B1ol-104481994597834/",
-        url_start_program:"https://shiftnetwork.infusionsoft.app/app/orderForms/210d9539-00d2-452a-b204-9cc7f7a53487?cookieUUID=ddcd50bd-7fdd-47cb-b354-4bd720157b3a",
+        url_start_program:"https://shiftnetwork.infusionsoft.app/app/orderForms/210d9539-00d2-452a-b204-9cc7f7a53487",
         contactForm:{
             field01 : "Nombre ",
             field01_message_null:"Por favor ingrese el Nombre Completo",
