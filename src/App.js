01f966b3944f4b8cc65c4fa3437ab0acdf2d9da8
projas <projas@kometsales.com>
2020-06-12 14:20:12 -0500

    oferta12

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import shamanic02 from './shamanic/page02'
 import shamanic03 from './shamanic/page03'
 import shamanic04 from './shamanic/page04'
 import shamanic05 from './shamanic/page05'
+import shamanic06 from './shamanic/page06'
 
 
 import store from './store'
@@ -25,17 +26,17 @@ const App = () => (
                 
                 <Route exact path="/practicas-chamanicas/oferta02" component={shamanic01} />
                 <Route exact path="/practicas-chamanicas/oferta01" component={shamanic01} />
+
                 <Route exact path="/gabriel" component={shamanic01} />
                 
                 <Route exact path="/practicas-chamanicas/oferta11" component={shamanic03} />
+                <Route exact path="/practicas-chamanicas/oferta12" component={shamanic06} />
+                
                 <Route exact path="/practicas-chamanicas/intro11" component={shamanic04} />
                 <Route exact path="/practicas-chamanicas/intro12" component={shamanic05} />
-
-
                 <Route exact path="/practicas-chamanicas/intro01" component={shamanic02} />
                 <Route exact path="/practicas-chamanicas/intro02" component={shamanic02} />
                 <Route exact path="/gabriel-intro" component={shamanic02} />
-
 
                 <Route component={shamanic01} />            
             </Switch>
