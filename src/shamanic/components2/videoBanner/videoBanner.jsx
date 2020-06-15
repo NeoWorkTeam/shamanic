e018df8767ce0c81342d75c3e66c1af70557961a
projas <projas@kometsales.com>
2020-06-15 16:25:51 -0500

    executed npm react scripts

--- src/shamanic/components2/videoBanner/videoBanner.jsx
+++ src/shamanic/components2/videoBanner/videoBanner.jsx
@@ -4,7 +4,7 @@ import classes from '../../style2.css';
 import video from '../../img/banner.mp4'
 
 const BackgroundVideo = () => {
-    const videoSource = "https://www.w3schools.com/tags/movie.mp4"
+
     return (
         <div className={classes.Container} >
             <video autoPlay="autoplay" loop="loop" muted className="Video" >
